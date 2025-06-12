044fe0c216458d4ee682a9e6377d6a0f9c98e9c6
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-12 23:42:15 +0800

    use pinata in production

--- packages/front-end/app/lib/ipfs-action.ts
+++ packages/front-end/app/lib/ipfs-action.ts
@@ -1,31 +1,46 @@
 import { create } from 'kubo-rpc-client';
 import { ApiError } from 'next/dist/server/api-utils';
 import toBuffer from 'it-to-buffer';
-const client = create(
-	process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-		? '/ip4/127.0.0.1/tcp/5001'
-		: 'https://ipfs.io',
-);
+import { PinataError, PinataSDK } from 'pinata';
+const pinata =
+	process.env.NODE_ENV === 'production'
+		? new PinataSDK({
+				pinataJwt: `${process.env.PINATA_JWT}`,
+				pinataGateway: `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`,
+				pinataGatewayKey: `${process.env.PINATA_GATEWAY_KEY}`,
+			})
+		: null;
+const kurboClient = create(process.env.IPFS_RPC_URL);
 export async function uploadFile(file: File): Promise<string> {
-	const res = await client.add({ content: file });
-	const cid = res.cid.toString();
-	if (
-		process.env.NODE_ENV === 'development' ||
-		process.env.NODE_ENV === 'test'
-	) {
-		await client.files.cp(
+	if (process.env.NODE_ENV === 'production') {
+		if (!pinata) {
+			throw new PinataError('pinata sdk creation failed');
+		}
+		const { cid } = await pinata.upload.public.file(file);
+		return cid;
+	} else {
+		const res = await kurboClient.add({ content: file });
+		const cid = res.cid.toString();
+		await kurboClient.files.cp(
 			res.cid,
 			`/${res.path}.${file.type.split('/')[1]}`,
 		);
+		return cid;
 	}
-	return cid;
 }
 export async function getFileByCid(cid: string | undefined): Promise<Blob> {
 	console.log('this is the cid:', cid);
 	if (cid === undefined) {
 		throw new ApiError(400, 'cid should not be null');
 	}
-	const res = client.cat(cid);
-	const ret = new Blob([await toBuffer(res)]);
-	return ret;
+	if (process.env.NODE_ENV === 'production') {
+		if (!pinata) {
+			throw new PinataError('pinata sdk creation failed');
+		}
+		return (await pinata?.gateways.public.get(cid))!.data as Blob;
+	} else {
+		const res = kurboClient.cat(cid);
+		const ret = new Blob([await toBuffer(res)]);
+		return ret;
+	}
 }
