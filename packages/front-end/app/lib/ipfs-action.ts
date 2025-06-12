0c123298aaa3c18a88fea7b07e46a96712a74b0b
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-12 20:30:24 +0800

    use public gateway when in production

--- packages/front-end/app/lib/ipfs-action.ts
+++ packages/front-end/app/lib/ipfs-action.ts
@@ -1,11 +1,23 @@
 import { create } from 'kubo-rpc-client';
 import { ApiError } from 'next/dist/server/api-utils';
 import toBuffer from 'it-to-buffer';
-const client = create('/ip4/127.0.0.1/tcp/5001');
+const client = create(
+	process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+		? '/ip4/127.0.0.1/tcp/5001'
+		: 'https://ipfs.io',
+);
 export async function uploadFile(file: File): Promise<string> {
 	const res = await client.add({ content: file });
 	const cid = res.cid.toString();
-	await client.files.cp(res.cid, `/${res.path}.${file.type.split('/')[1]}`);
+	if (
+		process.env.NODE_ENV === 'development' ||
+		process.env.NODE_ENV === 'test'
+	) {
+		await client.files.cp(
+			res.cid,
+			`/${res.path}.${file.type.split('/')[1]}`,
+		);
+	}
 	return cid;
 }
 export async function getFileByCid(cid: string | undefined): Promise<Blob> {
