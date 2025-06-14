32889a2c64491433475d364bc368918c7ea3f52b
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-14 14:06:42 +0800

    smart contract package export

---
+++ packages/smart-contract/src/index.ts
@@ -0,0 +1,5 @@
+import OrderRegistry from "../build/artifacts/contracts/OrderRegistry.sol/OrderRegistry.json";
+import ProductRegistry from "../build/artifacts/contracts/ProductRegistry.sol/ProductRegistry.json";
+import USDT from "../build/artifacts/contracts/USDT.sol/USDT.json";
+import sepoliaUSDT from "./sepoliaUSDT.json";
+export { OrderRegistry, ProductRegistry, USDT, sepoliaUSDT };
