386791ea35792042166893acfe18fbf85f8b588f
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-14 14:47:59 +0800

    remove add product type from distributor

--- packages/front-end/app/dashboard/warehouse/page.tsx
+++ packages/front-end/app/dashboard/warehouse/page.tsx
@@ -3,23 +3,40 @@ import { Suspense } from 'react';
 import { Button, Skeleton } from 'antd';
 import ProductTableWrapper from './product-table-wrapper';
 import Link from 'next/link';
+import { auth } from '@/auth';
+import prisma from '@/app/lib/prisma';
+import { Role } from '@/generated/prisma';
+import { AuthError } from 'next-auth';
 export const metadata: Metadata = {
 	title: 'Warehouse',
 };
 export default async function Page() {
+	const session = await auth();
+	if (!session?.user?.email) {
+		throw new AuthError('未登录或 session 信息不完整');
+	}
+	const user = await prisma.users.findUnique({
+		where: { email: session.user.email },
+		include: { foundedCompany: true },
+	});
+	if (user === null) {
+		throw new AuthError('未登录或 session 信息不完整');
+	}
 	return (
 		<div className="w-full">
 			<div className="flex justify-between items-center mb-6">
 				<h1 className="text-2xl font-bold">库存管理</h1>
 			</div>
-			<div className="flex justify-end mb-4">
-				<Link href="/dashboard/product_type/create">
-					<Button type="primary">新增商品</Button>
-				</Link>
-			</div>
+			{user?.role === Role.MANUFACTURER ? (
+				<div className="flex justify-end mb-4">
+					<Link href="/dashboard/product_type/create">
+						<Button type="primary">新增商品</Button>
+					</Link>
+				</div>
+			) : null}
 			{/* 将 products 传给前端组件 */}
 			<Suspense fallback={<Skeleton />}>
-				<ProductTableWrapper />
+				<ProductTableWrapper user={user} />
 			</Suspense>
 		</div>
 	);
--- packages/front-end/app/dashboard/warehouse/product-table-wrapper/index.tsx
+++ packages/front-end/app/dashboard/warehouse/product-table-wrapper/index.tsx
@@ -1,17 +1,13 @@
-import prisma from '@/app/lib/prisma';
 import ProductTable from '@/app/ui/dashboard/warehouse/productTable';
-import { auth } from '@/auth';
 import { fetchProductIsOnChain, fetchUserProductTypes } from '@/app/lib/data';
-export default async function ProductTableWrapper({ take }: { take?: number }) {
-	const session = await auth();
-	if (!session?.user?.email) {
-		throw new Error('未登录或 session 信息不完整');
-	}
-	// 根据用户邮箱查公司 ID
-	const user = await prisma.users.findUnique({
-		where: { email: session.user.email },
-		include: { foundedCompany: true },
-	});
+import { companies, users } from '@/generated/prisma';
+export default async function ProductTableWrapper({
+	take,
+	user,
+}: {
+	take?: number;
+	user: users & { foundedCompany: companies[] };
+}) {
 	let product_types = await fetchUserProductTypes(user!);
 	if (take) {
 		product_types = product_types.slice(0, take);
