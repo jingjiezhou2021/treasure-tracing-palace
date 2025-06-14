25a63556dc0916e29b87e45355a9d4e96874b600
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-14 17:10:16 +0800

    order detail skeleton

--- packages/front-end/app/ui/loading.tsx
+++ packages/front-end/app/ui/loading.tsx
@@ -1,7 +1,9 @@
+import { Spin } from 'antd';
+
 export default function Loading() {
 	return (
 		<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-			Loading...
+			<Spin size="large" />
 		</div>
 	);
 }
