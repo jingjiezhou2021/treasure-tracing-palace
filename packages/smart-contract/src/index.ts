67da41999531af7891a48e50bc88e252ec0dd4e1
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-14 15:36:26 +0800

    sepolia usdt wrapped in object

--- packages/smart-contract/src/index.ts
+++ packages/smart-contract/src/index.ts
@@ -1,5 +1,6 @@
 import OrderRegistry from "../build/artifacts/contracts/OrderRegistry.sol/OrderRegistry.json";
 import ProductRegistry from "../build/artifacts/contracts/ProductRegistry.sol/ProductRegistry.json";
 import USDT from "../build/artifacts/contracts/USDT.sol/USDT.json";
-import sepoliaUSDT from "./sepoliaUSDT.json";
+import sepoliaUSDTABI from "./sepoliaUSDT.json";
+const sepoliaUSDT = { abi: sepoliaUSDTABI };
 export { OrderRegistry, ProductRegistry, USDT, sepoliaUSDT };
