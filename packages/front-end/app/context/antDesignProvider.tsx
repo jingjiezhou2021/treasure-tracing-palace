e49dff953d8e2f0717b788f926c3bc4c4f3e57a5
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-16 02:26:40 +0800

    antd i18nized

--- packages/front-end/app/context/antDesignProvider.tsx
+++ packages/front-end/app/context/antDesignProvider.tsx
@@ -1,15 +1,21 @@
 'use client';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { ConfigProvider } from 'antd';
-import { Web3ConfigProvider } from '@ant-design/web3';
+import { en_US, Web3ConfigProvider, zh_CN } from '@ant-design/web3';
 import { ReactNode } from 'react';
+import { useParams } from 'next/navigation';
 export default function AntDesignCustomConfigProvider({
 	children,
 }: {
 	children: ReactNode;
 }) {
+	const lng = useParams()?.lng as 'zh' | 'en';
+	const lngMap = {
+		zh: zh_CN,
+		en: en_US,
+	};
 	return (
-		<Web3ConfigProvider>
+		<Web3ConfigProvider locale={lngMap[lng]}>
 			<AntdRegistry>
 				<ConfigProvider
 					theme={{
