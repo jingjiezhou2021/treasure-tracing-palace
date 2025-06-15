7bb2da9acc5d63350f001fd49391357a45e480ed
jingjiezhou2021 <musketeerdt@icloud.com>
2025-06-16 02:34:40 +0800

    antd language option typed

--- packages/front-end/app/context/antDesignProvider.tsx
+++ packages/front-end/app/context/antDesignProvider.tsx
@@ -1,16 +1,17 @@
 'use client';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { ConfigProvider } from 'antd';
-import { en_US, Web3ConfigProvider, zh_CN } from '@ant-design/web3';
+import { en_US, Locale, Web3ConfigProvider, zh_CN } from '@ant-design/web3';
 import { ReactNode } from 'react';
 import { useParams } from 'next/navigation';
+import { languages } from '../i18n/settings';
 export default function AntDesignCustomConfigProvider({
 	children,
 }: {
 	children: ReactNode;
 }) {
-	const lng = useParams()?.lng as 'zh' | 'en';
-	const lngMap = {
+	const lng = useParams()?.lng as (typeof languages)[number];
+	const lngMap: Record<typeof lng, Locale> = {
 		zh: zh_CN,
 		en: en_US,
 	};
